cd4ff215ba224630b08a4ba2856f6b0d0bf2cb2c
maninlov <128026663+11am-spy@users.noreply.github.com>
2026-05-30 20:30:40 +0700

    script.js

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const title = document.getElementById("title");
 const button = document.getElementById("btn");
 const button2 = document.getElementById("btn2");
 const gambar = document.getElementById("gambar");
+const audio = document.getElementById("audio");
+const gif = document.getElementById("gif");
 
 
 button.addEventListener("click", function (){
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
-    title.textContent = "Kamu Tertipu bangsuyyyy🤪";
-    gambar.src = "https://i.scdn.co/image/ab67616d0000b273c189ea3132793eec3fb32d85";
+    title.textContent = "Kicau Mania Dulu Ga Seh🤪";
+    gambar.src = "https://media.tenor.com/OoG1CF2T3QIAAAAj/kucing-scuba-scuba-cat.gif";
+    audio.play();
+    gif.src = "kucing.gif";
+    gif.style.display = "block";
+
+    
 
 });
 
